59e54870a3a4a8ed22bcdb474ea81dea54ab9d73
Ahmad Irfan Mohamamd Shukri <nerve2009@yahoo.com>
2021-07-30 02:27:22 +0800

    fetch blog's data from dummy_db.json with useEffect

--- src/components/BlogList.js
+++ src/components/BlogList.js
@@ -1,4 +1,4 @@
-const BlogList = ({ blogs, title, handleDelete }) => {
+const BlogList = ({ blogs, title }) => {
 	return (
 		<div className="blog-list">
 			<h1>{title}</h1>
@@ -6,7 +6,6 @@ const BlogList = ({ blogs, title, handleDelete }) => {
 				<div key={blog.id} className="blog-preview">
 					<h2>{blog.title ?? title}</h2>
 					<p>{blog.author}</p>
-					<button onClick={() => handleDelete(blog.id)}>Delete</button>
 				</div>
 			))}
 		</div>
