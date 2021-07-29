3197933036d59b6df2887bc05d89356aa707d418
Ahmad Irfan Mohamamd Shukri <nerve2009@yahoo.com>
2021-07-29 23:27:35 +0800

    reused component - with filtered feature

--- src/components/BlogList.js
+++ src/components/BlogList.js
@@ -1,9 +1,10 @@
-const BlogList = ({ blogs }) => {
+const BlogList = ({ blogs, title }) => {
 	return (
 		<div className="blog-list">
+			<h1>{title}</h1>
 			{blogs.map((blog) => (
 				<div key={blog.id} className="blog-preview">
-					<h2>{blog.title}</h2>
+					<h2>{blog.title ?? title}</h2>
 					<p>{blog.author}</p>
 				</div>
 			))}
