f1d6b54796c54292d720d46850fc97d72ae7028a
Ahmad Irfan Mohamamd Shukri <nerve2009@yahoo.com>
2021-07-30 22:12:42 +0800

    blog details page

--- src/components/BlogList.js
+++ src/components/BlogList.js
@@ -1,11 +1,15 @@
+import { Link } from "react-router-dom";
+
 const BlogList = ({ blogs, title }) => {
 	return (
 		<div className="blog-list">
-			<h1>{title}</h1>
+			<h2>{title}</h2>
 			{blogs.map((blog) => (
 				<div key={blog.id} className="blog-preview">
-					<h2>{blog.title ?? title}</h2>
-					<p>{blog.author}</p>
+					<Link to={`/blogs/${blog.id}`}>
+						<h2>{blog.title}</h2>
+						<p>Written by: {blog.author}</p>
+					</Link>
 				</div>
 			))}
 		</div>
