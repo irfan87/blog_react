1a2e6d850f186fea5b5835a00c54bc38ea8cdf91
Ahmad Irfan Mohamamd Shukri <nerve2009@yahoo.com>
2021-07-30 21:09:07 +0800

    use react-router-dom to route the pages

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import CreateBlog from "./components/CreateBlog";
+
 import Home from "./components/Home";
 import Navbar from "./components/Navbar";
 
 function App() {
 	return (
-		<div className="App">
-			{/* define navbar component here */}
-			<Navbar />
-			<div className="content">
-				<Home />
+		<Router>
+			<div className="App">
+				{/* define navbar component here */}
+				<Navbar />
+				<div className="content">
+					<Switch>
+						<Route component={Home} exact path="/" />
+						<Route component={CreateBlog} path="/create" />
+					</Switch>
+				</div>
 			</div>
-		</div>
+		</Router>
 	);
 }
 
